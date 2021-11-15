3b8597bb44b837acbdf22f38f0b8a1118a76c7f0
ashimkarki7 <asim.karki.ak8@gmail.com>
2021-11-15 15:10:34 +0545

    vueCdnCh1

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listElUL = document.querySelector('ul');
-function addGoal (){
-    const enteredValue = inputEl.value;
-    const listItemEl=document.createElement('li');
-listItemEl.textContent = enteredValue;
-listElUL.appendChild(listItemEl)
-}
+// Vue.createApp({
+//     data: function(){
 
-buttonEl.addEventListener('click', addGoal);
+//     }
+// })
+Vue.createApp({
+    data(){
+        return{
+goals:[],
+enteredValue: ''
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue)
+this.enteredValue =''
+        }
+    }
+}).mount('#app');
+// const vm = app.mount('#app')
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listElUL = document.querySelector('ul');
+// function addGoal (){
+//     const enteredValue = inputEl.value;
+//     const listItemEl=document.createElement('li');
+// listItemEl.textContent = enteredValue;
+// listElUL.appendChild(listItemEl)
+// inputEl.value
+// }
+
+// buttonEl.addEventListener('click', addGoal);
